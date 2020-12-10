ad892d34e08613da41127bc1f42829168e088379
damiisdandy <damilola.jerugba@gmail.com>
2020-12-10 13:17:28 -0800

    removed paystack in utils/index

--- utils/index.js
+++ utils/index.js
@@ -1,13 +1,11 @@
 const sendMail = require('./mail');
 const Server = require('./server');
 const axios = require('./axios');
-const paystack = require('./paystack');
 const tokenGenerator = require('./tokenGenerator');
 
 module.exports = {
   sendMail,
   Server,
   axios,
-  paystack,
   tokenGenerator,
 };
