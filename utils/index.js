8b67a7854a5eec1f745a22bac06150cda3b20d7c
damiisdandy <damilola.jerugba@gmail.com>
2020-12-10 13:18:45 -0800

    removed axios

--- utils/index.js
+++ utils/index.js
@@ -1,11 +1,9 @@
 const sendMail = require('./mail');
 const Server = require('./server');
-const axios = require('./axios');
 const tokenGenerator = require('./tokenGenerator');
 
 module.exports = {
   sendMail,
   Server,
-  axios,
   tokenGenerator,
 };
